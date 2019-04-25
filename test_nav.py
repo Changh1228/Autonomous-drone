601bc58a10d521c2564ae0f48aa4e6ed5bda4d31
Renaud Tahon <renaud.tahon@mic-belgique.be>
2019-04-25 23:30:04 +0200

    Latest version

--- test_nav.py
+++ test_nav.py
@@ -13,35 +13,20 @@ from crazyflie_driver.msg import Position, Hover
 from std_msgs.msg import Header
 
 class DroneMovement:
-    '''
-    a = [1.5, -0.75, 135]
-    b = [0.5, 0.25, 135]
-    c = [-1.25, 1.00, 180]
-    d = [-2.75, 0.5, 180]
-    e = [-2.5, -0.5, -90]
-    f = [-1.75, -0.75, 0]
-    g = [0.00, -0.75, 45]
-    h = [1.00, 0.25, 45]
-    '''
-
     def __init__(self, argv=sys.argv):
         rospy.init_node("drone_movement")
         rospy.loginfo("Starting DroneMovement.")
 
         self.path = []
-        self.path_ids = []
 
-        self.threshold = 0.025
+        self.threshold = 0.01
         self.degree_threshold = 5
 
         self.clearance_distance = 0.8
 
-        self.waiting_time = 5 # seconds
-        self.starting_time = 0.0
-
         self.goal = None
         self.last_position_command = None
-        self.current_target = 0
+        self.current_target = 2
         self.passing_gate = False
 
         self.checkpoints = []
@@ -64,9 +49,10 @@ class DroneMovement:
 
         # Create a checkpoint in front of each gate
         self.path = [Position(Header(), g["position"][0] + self.clearance_distance / 2 * np.cos(np.deg2rad(180 + g["heading"])), g["position"][1] + self.clearance_distance / 2 * np.sin(np.deg2rad(180 + g["heading"])), 0.41, g["heading"]) for g in world["gates"]]
-        self.path_ids = [g["id"] for g in world["gates"]]
-        
-        starting_time = timer()
+        self.gates = [[g["position"][0] - np.cos(np.deg2rad(g["heading"] - 90)) * 0.3, g["position"][1] - np.sin(np.deg2rad(g["heading"] - 90)) * 0.3, g["position"][0] + np.cos(np.deg2rad(g["heading"] - 90)) * 0.3, g["position"][1] + np.sin(np.deg2rad(g["heading"] - 90)) * 0.3] for g in world["gates"]]
+        self.walls = [[w["plane"]["start"][0], w["plane"]["start"][1], w["plane"]["stop"][0], w["plane"]["stop"][1]] for w in world["walls"]]
+
+        self.starting_time = timer()
         rospy.loginfo("Node finished initialization")
 
     #----------------------------------------------------#
@@ -74,62 +60,80 @@ class DroneMovement:
     #----------------------------------------------------#
 
     def plan_path(self, current_position, target_position):
-        path_x, path_y, path_yaw = a_star.aStarPlanning(current_position.x, current_position.y, target_position.x, target_position.y)
+        path_x, path_y, path_z, path_yaw = a_star.aStarPlanning(current_position.x, current_position.y, target_position.x, target_position.y)
         self.checkpoints = []
 
         for i in range(len(path_x)):
-            '''
-            if i + 1 < len(path_x):
-                diff = abs(path_yaw[i] - path_yaw[i + 1])
-                thresh = 30
-                step_size = 30
+            if i < len(path_x) - 1:
+                if path_z[i] != path_z[i + 1]:
+                    checkpoint = Position()
+                    checkpoint.x = path_x[i]
+                    checkpoint.y = path_y[i]
+                    checkpoint.z = path_z[i]
+                    checkpoint.yaw = path_yaw[i]
+                    self.checkpoints.append(checkpoint)
 
-                if diff >= thresh:
-                    steps = int(diff / step_size)
+                    # Add same point with next checkpoint height to level the drone before moving
+                    additional_checkpoint = Position()
+                    additional_checkpoint.x = path_x[i]
+                    additional_checkpoint.y = path_y[i]
+                    additional_checkpoint.z = path_z[i + 1]
+                    additional_checkpoint.yaw = path_yaw[i]
+                    self.checkpoints.append(additional_checkpoint)
+                else:
+                    checkpoint = Position()
+                    checkpoint.x = path_x[i]
+                    checkpoint.y = path_y[i]
+                    checkpoint.z = path_z[i]
+                    checkpoint.yaw = path_yaw[i]
 
-                    for s in range(steps):
-                        checkpoint = Position()
-                        checkpoint.x = path_x[i]
-                        checkpoint.y = path_y[i]
-                        checkpoint.z = 0.41
-
-                        if path_yaw[i] > path_yaw[i + 1]:
-                            checkpoint.yaw = path_yaw[i] - s * step_size
-                        else:
-                            checkpoint.yaw = path_yaw[i] + s * step_size
-
-                        self.checkpoints.append(checkpoint)
-
-                    if (diff % step_size) != 0:
-                        checkpoint = Position()
-                        checkpoint.x = path_x[i]
-                        checkpoint.y = path_y[i]
-                        checkpoint.z = 0.41
-                        checkpoint.yaw = path_yaw[i + 1]
-
-                        self.checkpoints.append(checkpoint)
+                    self.checkpoints.append(checkpoint)
             else:
                 checkpoint = Position()
                 checkpoint.x = path_x[i]
                 checkpoint.y = path_y[i]
-                checkpoint.z = 0.41
+                checkpoint.z = path_z[i]
                 checkpoint.yaw = path_yaw[i]
 
                 self.checkpoints.append(checkpoint)
-            '''
-
-            checkpoint = Position()
-            checkpoint.x = path_x[i]
-            checkpoint.y = path_y[i]
-            checkpoint.z = 0.41
-            checkpoint.yaw = path_yaw[i]
-
-            self.checkpoints.append(checkpoint)
         
         self.goal = self.checkpoints[0]
         self.passing_gate = True
 
         print("Plan finished")
+        print(self.checkpoints)
+
+    def determinant(self, v1, v2, v3, v4):
+        return (v1*v4-v2*v3)
+
+    def check_if_no_obstacle(self, start_x, start_y, goal_x, goal_y):
+        for g in range(len(self.gates)):
+            delta = self.determinant(start_x-goal_x, self.gates[g][0]-self.gates[g][2], start_y-goal_y, self.gates[g][1]-self.gates[g][3])
+            if delta > 0.000001 or delta < -0.000001:
+                return False
+
+            namenda = self.determinant(self.gates[g][0]-goal_x, self.gates[g][0]-self.gates[g][2], self.gates[g][1]-goal_y, self.gates[g][1]-self.gates[g][3]) / delta
+            if namenda <= 1 and namenda >= 0:
+                return False
+            
+            miu = self.determinant(start_x-goal_x, self.gates[g][0]-goal_x, start_y-goal_y, self.gates[g][1]-goal_y) / delta
+            if miu <= 1 and miu >= 0:
+                return False
+
+        for w in range(len(self.walls)):
+            delta = self.determinant(start_x-goal_x, self.walls[w][0]-self.walls[w][2], start_y-goal_y, self.walls[w][1]-self.walls[w][3])
+            if delta > 0.000001 or delta < -0.000001:
+                return False
+
+            namenda = self.determinant(self.walls[w][0]-goal_x, self.walls[w][0]-self.walls[w][2], self.walls[w][1]-goal_y, self.walls[w][1]-self.walls[w][3]) / delta
+            if namenda <= 1 and namenda >= 0:
+                return False
+            
+            miu = self.determinant(start_x-goal_x, self.walls[w][0]-goal_x, start_y-goal_y, self.walls[w][1]-goal_y) / delta
+            if miu <= 1 and miu >= 0:
+                return False
+        
+        return True
 
     #----------------------------------------------------#
     #                   Main functions                   #
@@ -218,71 +222,50 @@ class DroneMovement:
         detected_position.z = msg_map.pose.position.z
         detected_position.yaw = yaw
 
-        print("expected position map: " + str(self.goal))
-        print("current position map: " + str(detected_position))
+        # print("expected position map: " + str(self.goal))
+        # print("current position map: " + str(detected_position))
 
         if self.goal is None:
             self.plan_path(detected_position, self.path[self.current_target])
         else:
             anglediff = (detected_position.yaw - self.goal.yaw + 180 + 360) % 360 - 180
+            idx = self.checkpoints.index(self.goal) if self.goal in self.checkpoints else -1
 
             if self.goal.x + self.threshold > detected_position.x > self.goal.x - self.threshold and self.goal.y + self.threshold > detected_position.y > self.goal.y - self.threshold and self.goal.z + self.threshold > detected_position.z > self.goal.z - self.threshold and anglediff <= self.degree_threshold and anglediff >= -self.degree_threshold:
             # if self.goal.x + self.threshold > detected_position.x > self.goal.x - self.threshold and self.goal.y + self.threshold > detected_position.y > self.goal.y - self.threshold and self.goal.z + self.threshold > detected_position.z > self.goal.z - self.threshold:
-                idx = self.checkpoints.index(self.goal) if self.goal in self.checkpoints else -1
+                print(idx)
+                
                 if idx != -1 and idx < len(self.checkpoints) - 1:
                     self.goal = self.checkpoints[idx + 1]
                     print("Next checkpoint")
-                    print(self.goal.yaw)
                 else:
                     if self.passing_gate:
-                        angle_to_pass_gate = np.deg2rad(self.path[self.current_target].yaw)
-                        
-                        other_side_of_gate = Position()
-                        other_side_of_gate.x = detected_position.x + self.clearance_distance * np.cos(angle_to_pass_gate)
-                        other_side_of_gate.y = detected_position.y + self.clearance_distance * np.sin(angle_to_pass_gate)
-                        other_side_of_gate.z = 0.41
-                        # other_side_of_gate.yaw = self.path[self.current_target].yaw
-                        other_side_of_gate.yaw = self.goal.yaw
-
-                        print(other_side_of_gate)
-
-                        '''
-                        self.checkpoints = []
-
-                        if abs(detected_position.x - other_side_of_gate.x) > abs(detected_position.y - other_side_of_gate.y):
-                            steps = int(abs(detected_position.x - other_side_of_gate.x) / 0.2)
-
-                            for s in range(steps):
-                                checkpoint = Position()
-                                checkpoint.x = detected_position.x + ((other_side_of_gate.x - detected_position.x) / steps) * (s + 1)
-                                checkpoint.y = detected_position.y + ((other_side_of_gate.y - detected_position.y) / steps) * (s + 1)
-                                checkpoint.z = 0.4
-                                checkpoint.yaw = other_side_of_gate.yaw
-
-                                self.checkpoints.append(checkpoint)
+                        if self.goal.z == 0.41:
+                            angle_to_pass_gate = np.deg2rad(self.path[self.current_target].yaw)
                             
-                            self.goal = self.checkpoints[0]
+                            other_side_of_gate = Position()
+                            other_side_of_gate.x = detected_position.x + self.clearance_distance * np.cos(angle_to_pass_gate)
+                            other_side_of_gate.y = detected_position.y + self.clearance_distance * np.sin(angle_to_pass_gate)
+                            other_side_of_gate.z = 0.41
+                            # other_side_of_gate.yaw = self.path[self.current_target].yaw
+                            other_side_of_gate.yaw = self.goal.yaw
+
+                            self.goal = other_side_of_gate
+                            self.current_target = self.current_target + 1
+                            self.passing_gate = False
+                            print("Passing gate")
                         else:
-                            steps = int(abs(detected_position.y - other_side_of_gate.y) / 0.2)
-
-                            for s in range(steps):
-                                checkpoint = Position()
-                                checkpoint.x = detected_position.x + ((other_side_of_gate.x - detected_position.x) / steps) * (s + 1)
-                                checkpoint.y = detected_position.y + ((other_side_of_gate.y - detected_position.y) / steps) * (s + 1)
-                                checkpoint.z = 0.4
-                                checkpoint.yaw = other_side_of_gate.yaw
-
-                                self.checkpoints.append(checkpoint)
-                            
-                            self.goal = self.checkpoints[0]
-                        '''
-
-                        self.goal = other_side_of_gate
-                        self.current_target = self.current_target + 1
-                        self.passing_gate = False
-                        print("Passing gate")
+                            self.goal.z = 0.41
+                            print("Leveling drone before passing the gate")
                     else:
                         self.plan_path(detected_position, self.path[self.current_target])
+            else:
+                if idx != -1 and idx < len(self.checkpoints) - 1:
+                    next_goal = self.checkpoints[idx + 1]
+
+                    if abs(self.goal.x - detected_position.x) + abs(self.goal.y - detected_position.y) >= abs(next_goal.x - detected_position.x) + abs(next_goal.y - detected_position.y):
+                        if self.check_if_no_obstacle(detected_position.x, detected_position.y, next_goal.x, next_goal.y):
+                            self.goal = self.checkpoints[idx + 1]
 
 if __name__ == "__main__":
     ros_node = DroneMovement()
@@ -290,9 +273,7 @@ if __name__ == "__main__":
     rate = rospy.Rate(20) # Hz
 
     while not rospy.is_shutdown():
-        if ros_node.starting_time != 0.0 and timer() - ros_node.starting_time > ros_node.waiting_time:
-            ros_node.hover_in_place(5.0)
-        elif ros_node.goal is None:
+        if ros_node.goal is None:
             ros_node.hover_in_place(0.0)
         else:
             ros_node.move()
